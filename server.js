d2104a0789d18c0875d6ef764aca6f746f08ec89
p.olshevskii <opeter@bk.ru>
2025-10-27 12:48:26 +0300

    update deps - add cors using

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const PORT = 3000;
 
 // Middleware для парсинга JSON
 app.use(express.json());
+app.use(cors());
 
 // Простые данные в памяти
 let users = [
